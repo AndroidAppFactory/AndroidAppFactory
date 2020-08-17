fecfc748532cb7301d9447cd32948784cc207be2
子勰 <code@bihe0832.com>
2020-08-17 10:27:08 +0800

    try

--- LibImage/src/main/java/com/bihe0832/android/lib/ui/image/GlideExt.kt
+++ LibImage/src/main/java/com/bihe0832/android/lib/ui/image/GlideExt.kt
@@ -1,7 +1,6 @@
 package com.bihe0832.android.lib.ui.image
 
 import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -38,52 +37,55 @@ fun ImageView.loadFitCenterImage(url: String, placeholder: Int = Color.GRAY, err
     loadImage(url, placeholder, error, RequestOptions().fitCenter())
 }
 
-fun ImageView.loadImage(
-        resId: Int,
-        requestOptions: RequestOptions = RequestOptions()
-) {
-    requestOptions
-            .dontAnimate()
-            .centerCrop()
-    Glide.with(this).load(resId).apply(requestOptions).thumbnail(0.3f).into(this)
+fun ImageView.loadImage(resId: Int) {
+    loadImage(resId, RequestOptions())
 }
 
 
-fun ImageView.loadImage(
-        resId: Int
-) {
-    Glide.with(this).load(resId).thumbnail(0.3f).into(this)
+fun ImageView.loadImage(resId: Int, requestOptions: RequestOptions = RequestOptions()) {
+    requestOptions.dontAnimate().centerCrop()
+    try {
+        Glide.with(this.context).load(resId).apply(requestOptions).thumbnail(0.3f).into(this)
+    } catch (e: Exception) {
+        e.printStackTrace()
+        try {
+            setImageResource(resId)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 }
 
+fun ImageView.loadImage(url: String, placeholder: Int = Color.GRAY, error: Int = placeholder) {
+    loadImage(url, placeholder, error, RequestOptions())
+}
 
-fun ImageView.loadImage(
-        url: String,
-        placeholder: Int,
-        error: Int,
-        requestOptions: RequestOptions = RequestOptions()
-) {
+fun ImageView.loadImage(url: String, placeholder: Int, error: Int, requestOptions: RequestOptions = RequestOptions()) {
     requestOptions
             .downsample(DownsampleStrategy.CENTER_INSIDE)//将图片缩小至目标大小
             .diskCacheStrategy(DiskCacheStrategy.RESOURCE)//换成变换后的图片
             .placeholder(placeholder)
             .error(error)
 
-    if (url.endsWith("gif")) {
-        Glide.with(this).asGif().load(url).thumbnail(0.3f).apply(requestOptions).into(this)
-    } else {
-        requestOptions.dontAnimate()
-        Glide.with(this).load(url).thumbnail(0.3f).apply(requestOptions).into(this)
+    try {
+        if (url.endsWith("gif")) {
+            Glide.with(this.context).asGif().load(url).thumbnail(0.3f).apply(requestOptions).into(this)
+        } else {
+            requestOptions.dontAnimate()
+            Glide.with(this.context).load(url).thumbnail(0.3f).apply(requestOptions).into(this)
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+        loadImage(error)
     }
 }
 
-fun ImageView.loadImage(
-        url: String,
-        placeholder: Int = Color.GRAY,
-        error: Int = placeholder
-) {
-    loadImage(url, placeholder, error, RequestOptions())
-}
 
 fun ImageView.clear() {
-    Glide.with(this).clear(this)
+    try {
+        Glide.with(this.context).clear(this)
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+
 }
