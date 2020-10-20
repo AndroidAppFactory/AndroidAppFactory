0b648108c90f2c6d954ab1248e90fa67dd38a5fc
子勰 <code@bihe0832.com>
2020-10-20 12:08:57 +0800

    特殊app的信息

--- LibAPK/src/main/java/com/bihe0832/android/lib/utils/apk/APKUtils.java
+++ LibAPK/src/main/java/com/bihe0832/android/lib/utils/apk/APKUtils.java
@@ -93,11 +93,6 @@ public class APKUtils {
         }
     }
 
-    public static final String APK_PACKAGE_NAME_WECHAT = "com.tencent.mm";
-    public static final String APK_LAUNCHER_CLASS_WECHAT = "com.tencent.mm.ui.LauncherUI";
-
-    public static final String APK_PACKAGE_NAME_QQ = "com.tencent.mobileqq";
-    public static final String APK_LAUNCHER_CLASS_QQ = "com.tencent.mobileqq.activity.HomeActivity";
 
     public static boolean startApp(Context ctx, String pkgName) {
         return startApp(ctx, pkgName, true);
---
+++ LibSuperAPP/src/main/java/com/bihe0832/android/lib/superapp/SuperAPPContants.java
@@ -0,0 +1,15 @@
+package com.bihe0832.android.lib.superapp;
+
+/**
+ * @author hardyshi code@bihe0832.com
+ * Created on 2020/10/20.
+ * Description: Description
+ */
+public class SuperAPPContants {
+    public static final String APK_PACKAGE_NAME_WECHAT = "com.tencent.mm";
+    public static final String APK_LAUNCHER_CLASS_WECHAT = "com.tencent.mm.ui.LauncherUI";
+
+    public static final String APK_PACKAGE_NAME_QQ = "com.tencent.mobileqq";
+    public static final String APK_LAUNCHER_CLASS_QQ = "com.tencent.mobileqq.activity.HomeActivity";
+
+}
