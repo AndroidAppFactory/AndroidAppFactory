c49926e27912b788b3754e29df03d0b372ef818f
子勰 <code@bihe0832.com>
2020-11-06 20:01:09 +0800

    安装优化

---
+++ LibDownloadAndInstall/src/main/java/com/bihe0832/android/lib/install/InstallErrorCode.java
@@ -0,0 +1,16 @@
+package com.bihe0832.android.lib.install;
+
+public class InstallErrorCode {
+    public static final int FILE_NOT_FOUND = -1;
+
+    public static final int START_SYSTEM_INSTALL_EXCEPTION = -2;
+
+    public static final int UNKNOWN_EXCEPTION = -3;
+
+    public static final int PERMISSION_DENY = -4;
+
+    public static final int UNZIP_FAILED = -5;
+
+    public static final int BAD_APK_TYPE = -6;
+
+}
