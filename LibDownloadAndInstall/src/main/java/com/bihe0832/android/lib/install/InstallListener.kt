6bb5452ea1422f88137230d4c4b242a991d27c80
子勰 <code@bihe0832.com>
2020-11-06 20:01:09 +0800

    安装优化

---
+++ LibDownloadAndInstall/src/main/java/com/bihe0832/android/lib/install/InstallListener.kt
@@ -0,0 +1,15 @@
+package com.bihe0832.android.lib.install
+
+/**
+ *
+ * @author zixie code@bihe0832.com
+ * Created on 2020-01-09.
+ * Description: Description
+ *
+ */
+interface InstallListener {
+    fun onUnCompress()
+    fun onInstallPrepare()
+    fun onInstallStart()
+    fun onInstallFailed(errorCode: Int)
+}
