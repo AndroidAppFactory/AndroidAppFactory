270970d1d701c17217eef116360278c091a1f058
子勰 <code@bihe0832.com>
2020-11-13 14:30:25 +0800

    adapter 及 卡片 重构，去中心化

---
+++ LibAdapter/src/main/java/com/bihe0832/android/lib/adapter/CardBaseInnerModule.java
@@ -0,0 +1,32 @@
+package com.bihe0832.android.lib.adapter;
+
+
+import android.text.TextUtils;
+
+import com.chad.library.adapter.base.entity.MultiItemEntity;
+
+/**
+ * Created by zixie on 2017/12/7.
+ * <p>
+ * 所有卡片对应的数据都继承自该类
+ */
+
+public abstract class CardBaseInnerModule implements MultiItemEntity {
+
+    private String mTypeBadText = "暂不支持的数据格式";
+
+    public CardBaseInnerModule(String typeBadText) {
+        if (!TextUtils.isEmpty(typeBadText)) {
+            mTypeBadText = typeBadText;
+        }
+    }
+
+    public abstract int getResID();
+
+    public abstract Class<? extends CardBaseHolder> getViewHolderClass();
+
+    public String getTypeBadText() {
+        return mTypeBadText;
+    }
+
+}
