5bbf27a4fe4b69d65e4684c983cfe52576ec9513
子勰 <code@bihe0832.com>
2020-11-15 16:11:14 +0800

    config 切换为MMKV

--- LibConfig/src/main/java/com/bihe0832/android/lib/config/ConfigManager.java
+++ LibConfig/src/main/java/com/bihe0832/android/lib/config/ConfigManager.java
@@ -1,11 +1,12 @@
 package com.bihe0832.android.lib.config;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.text.TextUtils;
 
 import com.bihe0832.android.lib.log.ZLog;
 import com.bihe0832.android.lib.utils.ConvertUtils;
+import com.tencent.mmkv.MMKV;
+import com.tencent.mmkv.MMKVLogLevel;
 
 import org.json.JSONObject;
 
@@ -13,11 +14,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author code@bihe0832.com
@@ -30,9 +31,9 @@ class ConfigManager {
     //配置文件配置
     private Properties mLocalConfig = null;
     //内存中的配置
-    private HashMap<String, String> mConfigInfoInCache = new HashMap<>();
-    //云端的配置
-    private SharedPreferences mConfigSP = null;
+    private ConcurrentHashMap<String, String> mConfigInfoInCache = new ConcurrentHashMap<>();
+
+    private MMKV mMMKVInstance = null;
 
     protected static ConfigManager getInstance() {
         if (instance == null) {
@@ -53,25 +54,26 @@ class ConfigManager {
         }
         try {
             loadFile(ctx, file, isDebug);
-            mConfigSP = ctx.getSharedPreferences(file.toUpperCase(), Context.MODE_PRIVATE);
-            if (isDebug) {
-                ZLog.d(TAG, "================== config ================");
-                ZLog.d(TAG, "local config:");
-                Set<Map.Entry<Object, Object>> entrySet = mLocalConfig.entrySet();//返回的属性键值对实体
-                for (Map.Entry<Object, Object> entry : entrySet) {
-                    ZLog.d(TAG, entry.getKey() + "=" + entry.getValue());
-                }
-                ZLog.d(TAG, "local config:");
-                Map<String, ?> allContent = mConfigSP.getAll();
-                for (Map.Entry<String, ?> entry : allContent.entrySet()) {
-                    ZLog.d(TAG, entry.getKey() + "=" + entry.getValue());
-                }
-                ZLog.d(TAG, "================== config ================");
-            }
         } catch (Exception e) {
             e.printStackTrace();
             ZLog.d(TAG, "ERROR: config file");
         }
+
+        try {
+            String rootDir = MMKV.initialize(ctx.getApplicationContext());
+            mMMKVInstance = MMKV.defaultMMKV();
+            if (isDebug) {
+                MMKV.setLogLevel(MMKVLogLevel.LevelInfo);
+            } else {
+                MMKV.setLogLevel(MMKVLogLevel.LevelNone);
+            }
+            ZLog.d(TAG, "mmkv root: " + rootDir);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            ZLog.d(TAG, "ERROR: config file");
+        }
+
     }
 
     protected void loadFile(Context ctx, String file, boolean isDebug) {
@@ -140,16 +142,15 @@ class ConfigManager {
 
     // 从云端推送保存到本地SharedPreferences的config，云端配置读取不使用默认值
     private String readCloudConfig(String key, String defValue) {
-        String value = null;
         try {
-            if (null != mConfigSP && mConfigSP.contains(key)) {
-                return mConfigSP.getString(key, null);
+            if (null != mMMKVInstance) {
+                return mMMKVInstance.decodeString(key, defValue);
             }
         } catch (Exception e) {
             e.printStackTrace();
             ZLog.d(TAG, "readCloudConfig failed");
         }
-        return value;
+        return defValue;
     }
 
     protected String readConfig(String key, String defValue) {
@@ -210,9 +211,19 @@ class ConfigManager {
         return ConvertUtils.parseDouble(configInterval, defaultValue);
     }
 
+    protected boolean writeConfig(String key, boolean value, boolean saveToLocal) {
+        return writeConfig(key, value ? Config.VALUE_SWITCH_ON : Config.VALUE_SWITCH_OFF, saveToLocal);
+    }
+
     protected boolean writeConfig(String key, String value, boolean saveToLocal) {
         ZLog.d(TAG, "writeConfig, key is :" + key + ";value is:" + value);
         try {
+
+            if (TextUtils.isEmpty(key)) {
+                ZLog.d(TAG, "writeConfig, key is null:" + key);
+                return false;
+            }
+
             if (TextUtils.isEmpty(value)) {
                 ZLog.d(TAG, "writeConfig, value is null:" + key);
                 value = "";
@@ -224,13 +235,11 @@ class ConfigManager {
             if (!saveToLocal) {
                 return true;
             } else {
-                if (null == mConfigSP) {
+                if (null == mMMKVInstance) {
                     ZLog.d(TAG, "writeConfig, sp is null:");
                     return false;
                 }
-                SharedPreferences.Editor editor = mConfigSP.edit();
-                editor.putString(key, value);
-                boolean result = editor.commit();
+                boolean result = mMMKVInstance.encode(key, value);
                 ZLog.d(TAG, "writeConfig result:" + result);
                 return result;
             }
@@ -238,10 +247,6 @@ class ConfigManager {
             e.printStackTrace();
             return false;
         }
-    }
-
-    protected boolean writeConfig(String key, boolean value, boolean saveToLocal) {
-        return writeConfig(key, value ? Config.VALUE_SWITCH_ON : Config.VALUE_SWITCH_OFF, saveToLocal);
     }
 
     protected boolean writeConfigs(Map<String, String> configs, boolean saveToLocal) {
@@ -253,33 +258,25 @@ class ConfigManager {
                 return false;
             }
 
-            if (null == mConfigSP && saveToLocal) {
+            if (null == mMMKVInstance && saveToLocal) {
                 ZLog.d(TAG, "writeConfig, sp is null:");
                 return false;
             }
-            SharedPreferences.Editor editor = null;
+
+
             if (configs != null && configs.size() > 0) {
-                if (saveToLocal) {
-                    editor = mConfigSP.edit();
-                }
-                Set<Map.Entry<String, String>> entrySet = configs.entrySet();
-                for (Map.Entry<String, String> entry : entrySet) {
+                for (Map.Entry<String, String> entry : configs.entrySet()) {
                     String key = entry.getKey();
                     String value = entry.getValue();
-                    if (!TextUtils.isEmpty(key) && !TextUtils.isEmpty(value)) {
-                        if (saveToLocal && null != editor) {
-                            editor.putString(key, value);
-                        }
+                    ZLog.d(TAG, "key :" + key + " ;value: " + value);
+                    boolean tempResult = writeConfig(key, value, saveToLocal);
+                    if (tempResult) {
                         mConfigInfoInCache.put(key, value);
+                    } else {
+                        result = false;
                     }
                 }
-                if (saveToLocal) {
-                    result = editor.commit();
-                    ZLog.d(TAG, "writeConfig result:" + result);
-                    return result;
-                } else {
-                    return true;
-                }
+                return result;
             } else {
                 ZLog.d(TAG, "writeConfig, configs is null:");
                 return false;
@@ -299,35 +296,25 @@ class ConfigManager {
                 return false;
             }
 
-            if (null == mConfigSP && saveToLocal) {
+            if (null == mMMKVInstance && saveToLocal) {
                 ZLog.d(TAG, "writeConfig, sp is null:");
                 return false;
             }
-            SharedPreferences.Editor editor = null;
             if (configs != null) {
-                if (saveToLocal) {
-                    editor = mConfigSP.edit();
-                }
                 Iterator<String> keys = configs.keys();
                 while (keys.hasNext()) {
                     // 获得key
                     String key = keys.next();
                     String value = configs.getString(key);
                     ZLog.d(TAG, "key :" + key + " ;value: " + value);
-                    if (!TextUtils.isEmpty(key) && !TextUtils.isEmpty(value)) {
-                        if (saveToLocal && null != editor) {
-                            editor.putString(key, value);
-                        }
+                    boolean tempResult = writeConfig(key, value, saveToLocal);
+                    if (tempResult) {
                         mConfigInfoInCache.put(key, value);
+                    } else {
+                        result = false;
                     }
                 }
-                if (saveToLocal) {
-                    result = editor.commit();
-                    ZLog.d(TAG, "writeConfig result:" + result);
-                    return result;
-                } else {
-                    return true;
-                }
+                return result;
             } else {
                 ZLog.d(TAG, "writeConfig, configs is null:");
                 return false;
