69808e80a1beb62305e58d46380b61b9c67658d4
子勰 <code@bihe0832.com>
2020-11-17 17:57:10 +0800

    debug 显示异常修复，通用列表支持两种样式

---
+++ Framework/src/main/java/com/bihe0832/android/framework/ui/list/easyrefresh/swiperefresh/CommonListFragment.kt
@@ -0,0 +1,58 @@
+package  com.bihe0832.android.framework.ui.list.easyrefresh.swiperefresh
+
+import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
+import com.bihe0832.android.framework.R
+import com.bihe0832.android.framework.ui.list.BaseListFragment
+import com.bihe0832.android.lib.adapter.CardBaseModule
+
+/**
+ * @author code@bihe0832.com
+ * Created on 2019-09-12.
+ * Description: 一款通用的包含一个recycleView的列表页，目前支持：下拉刷新、加载更多、添加header、列表为空展示空页面等
+ */
+abstract class CommonListFragment : BaseListFragment() {
+
+    private var mRefresh: SwipeRefreshLayout? = null
+
+
+    override fun getResID(): Int {
+        return R.layout.common_fragment_list_swipe
+    }
+
+    override fun initView(view: View) {
+        super.initView(view)
+        mRefresh = view.findViewById(R.id.fragment_list_refresh)
+        if (mDataLiveData.canRefresh()) {
+            mRefresh?.setOnRefreshListener {
+                mDataLiveData.clearData()
+                mDataLiveData.fetchData()
+            }
+        }
+
+        mRefresh?.setOnChildScrollUpCallback { _, _ ->
+            var headerNum = mAdapter.headerLayoutCount
+            (mRecyclerView?.childCount ?: 0 > 0
+                    && ((mRecyclerView?.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition() > headerNum || mRecyclerView?.getChildAt(0)
+                    ?.top ?: 0 < mRecyclerView?.paddingTop ?: 0))
+        }
+        mDataLiveData.fetchData()
+
+        mAdapter.setOnLoadMoreListener({
+            mDataLiveData.loadMore()
+        }, mRecyclerView)
+    }
+
+    override fun updateData(data: List<CardBaseModule>) {
+        super.updateData(data)
+        mRefresh?.isRefreshing = false
+        mAdapter.loadMoreComplete()
+        mAdapter.setEnableLoadMore(mDataLiveData.hasMore())
+    }
+
+    fun loadDataFinished() {
+        mRefresh?.isRefreshing = false
+        mAdapter.loadMoreComplete()
+    }
+}
