0f8762f5b5493b5a876286b85bef7a4734e712ec
子勰 <code@bihe0832.com>
2020-11-27 19:54:54 +0800

    代码同步

--- Application/src/main/java/com/bihe0832/android/app/router/RouterConstants.java
+++ Application/src/main/java/com/bihe0832/android/app/router/RouterConstants.java
@@ -1,7 +1,7 @@
 package com.bihe0832.android.app.router;
 
 
-import com.bihe0832.android.app.about.AboutActivity;
+import com.bihe0832.android.app.about.AboutActivityKt;
 import com.bihe0832.android.common.feedback.FeedbackActivity;
 import com.bihe0832.android.common.webview.BaseWebviewFragment;
 import com.bihe0832.android.common.webview.WebPageActivity;
@@ -11,9 +11,10 @@ import com.bihe0832.android.common.webview.WebPageActivity;
  */
 
 public class RouterConstants {
-    //用户反馈
-    public static final String MODULE_NAME_FEEDBACK = FeedbackActivity.MODULE_NAME_FEEDBACK;
+
     public static final String MODULE_NAME_WEB_PAGE = WebPageActivity.MODULE_NAME_WEB_PAGE;
+    public static final String MODULE_NAME_FEEDBACK = FeedbackActivity.MODULE_NAME_FEEDBACK;
     public static final String INTENT_EXTRA_KEY_WEB_URL = BaseWebviewFragment.INTENT_KEY_URL;
-    public static final String MODULE_NAME_BASE_ABOUT = AboutActivity.MODULE_NAME_BASE_ABOUT;
+    public static final String MODULE_NAME_BASE_ABOUT = AboutActivityKt.MODULE_NAME_BASE_ABOUT;
+
 }
