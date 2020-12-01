fe3c8a5049ea78273e8eb13b994a81c37c5c10eb
子勰 <code@bihe0832.com>
2020-12-01 16:53:55 +0800

    危险权限和权限组

--- Application/src/main/java/com/bihe0832/android/app/Application.java
+++ Application/src/main/java/com/bihe0832/android/app/Application.java
@@ -12,16 +12,16 @@ public class Application extends BaseApplication {
      * 是否为调试版本，请勿手动修改，自动构建会自动修改。如有问题，请联系hardy
      * 当IS_TEST_VERSION为true时，表示当前是开发版本
      */
-    private static final boolean IS_TEST_VERSION = false;
+    private static final boolean IS_TEST_VERSION = true;
 
     /**
      * 是否为正式发布版本，请勿手动修改，自动构建会自动修改。如有问题，请联系hardy
      * 当 IS_OFFICIAL_VERSION == true && IS_TEST_VERSION == false 时 不提示 [ showOfficial ] 其余都提示
      */
-    private static final boolean IS_OFFICIAL_VERSION = true;
+    private static final boolean IS_OFFICIAL_VERSION = false;
 
     //版本对应TAG，请勿手动修改，自动构建会自动修改。如有问题，请联系hardy
-    private static final String VERSION_TAG = "Tag_ZIXIE_1.0.1_1";
+    private static final String VERSION_TAG = "Tag_ZIXIE_1.0.0_1";
 
     static {
         try {
