ef787f560462263024fa0d001ac8e5525e1bc822
子勰 <code@bihe0832.com>
2020-12-01 20:25:52 +0800

    测试不自动刷新

--- APPTest/src/main/java/com/bihe0832/android/test/base/BaseTestActivity.kt
+++ APPTest/src/main/java/com/bihe0832/android/test/base/BaseTestActivity.kt
@@ -2,7 +2,6 @@ package com.bihe0832.android.test.base
 
 import android.os.Build
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.os.StrictMode
 import com.bihe0832.android.app.router.openWebPage
 import com.bihe0832.android.framework.ui.list.CommonListLiveData
@@ -10,16 +9,17 @@ import com.bihe0832.android.framework.ui.list.easyrefresh.CommonListActivity
 import com.bihe0832.android.lib.adapter.CardBaseModule
 import com.bihe0832.android.lib.debug.DebugTools
 import com.bihe0832.android.lib.debug.InputDialogCompletedCallback
-import com.bihe0832.android.test.R
 import com.bihe0832.android.test.base.item.TestItemData
 
 abstract class BaseTestActivity : CommonListActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         if (Build.VERSION.SDK_INT > 9) {
             StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().permitAll().build())
         }
     }
+
 
     open fun getDataList(): ArrayList<CardBaseModule> {
         return ArrayList<CardBaseModule>().apply {
@@ -39,11 +39,6 @@ abstract class BaseTestActivity : CommonListActivity() {
         return mTestDataLiveData
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        window.decorView.setBackgroundColor(resources.getColor(R.color.white))
-    }
-    
     fun showInputDialog(titleName: String, msg: String, defaultValue: String, listener: InputDialogCompletedCallback) {
         DebugTools.showInputDialog(this, titleName, msg, defaultValue, listener)
     }
