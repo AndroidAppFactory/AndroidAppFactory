786a02f7a3cb17acdd4ec38a2d2d5b3ca875dbda
子勰 <code@bihe0832.com>
2020-12-22 23:06:19 +0800

    注释整理

--- LibWebview/src/main/java/com/bihe0832/android/lib/web/WebViewService.java
+++ LibWebview/src/main/java/com/bihe0832/android/lib/web/WebViewService.java
@@ -5,16 +5,6 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
-/**
- * Copyright (c) 2018 Tencent. All rights reserved.
- *
- * @Project: app
- * @Package: com.tencent.jygame.app.webview
- * @Author: henryjpxie
- * @EMail: henryjpxie@tencent.com
- * @Date: 2018/7/4 11:25
- * @Version: V1.0
- */
 public class WebViewService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
