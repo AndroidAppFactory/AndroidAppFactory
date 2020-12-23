27747fdffb3f077d36f38684a0e32c046891fa48
子勰 <code@bihe0832.com>
2020-12-23 15:11:51 +0800

    文件直接下载

--- Framework/src/main/java/com/bihe0832/android/framework/request/ZixieRequestFile.kt
+++ Framework/src/main/java/com/bihe0832/android/framework/request/ZixieRequestFile.kt
@@ -9,18 +9,22 @@ import com.bihe0832.android.lib.utils.encypt.MD5
 /**
  *
  * 文件下载及校验
+ *  下载URL的格式为：URL;MD5，例如：http://blog.bihe0832.com;7478b16f5acd0a6febb7f7e3d9298f3d
+ *  下载以后默认会强校验MD5是否正确，如果关闭校验MD5
+ *  下载以后默认会保存本地，下次会优先尝试使用本地数据
+ *  支持下载的时候同时提供一个备用URL,如果主URL加载失败，可以尝试加载备用URL
  */
 
 class ZixieRequestFile {
 
-    private val TAG = "[MnaRequestFile] -> "
+    private val TAG = "[ZixieRequestFile] -> "
     private val CONFIG_SUFFIX_VALUE = "_value"
 
-    // 云控字段
-    private var configValue: String = ""
+    // 下载使用的主URL
+    private var fetchURLWithMD5: String = ""
 
-    // 云控字段的默认值
-    private var defaultValue: String = ""
+    // 下载使用的备选URL, 如果主URL加载失败，可以尝试加载备用URL
+    private var backupURLWithMD5: String = ""
 
     // 下载文件是否检查MD5
     private var forceCheckMD5: Boolean = true
@@ -53,24 +57,42 @@ class ZixieRequestFile {
         fun onFailed(errorCode: Int, msg: String)
     }
 
+    //无备份URL，支持MD5检查，使用本地缓存
+    fun get(configValue: String, callback: ResponseHandler) {
+        get(configValue, true, callback)
+    }
+
+    //无备份URL，MD5检查可自定义，使用本地缓存
+    fun get(configValue: String, forceCheckMD5: Boolean, callback: ResponseHandler) {
+        get(configValue, forceCheckMD5, true, callback)
+    }
+
+    //无备份URL，MD5检查、是否使用本地值可自定义
+    fun get(configValue: String, forceCheckMD5: Boolean, useCache: Boolean, callback: ResponseHandler) {
+        get(configValue, "", forceCheckMD5, useCache, callback)
+    }
+
+    //主URL拉取失败，尝试拉取备份URL，支持MD5检查，使用本地缓存
     fun get(configValue: String, defaultValue: String, callback: ResponseHandler) {
         get(configValue, defaultValue, true, callback)
     }
 
+    //主URL拉取失败，尝试拉取备份URL，MD5检查可自定义，使用本地缓存
     fun get(configValue: String, defaultValue: String, forceCheckMD5: Boolean, callback: ResponseHandler) {
         get(configValue, defaultValue, forceCheckMD5, true, callback)
     }
 
-    fun get(configValue: String, defaultValue: String, forceCheckMD5: Boolean, saveToLocal: Boolean, callback: ResponseHandler) {
-        this.configValue = configValue
-        this.defaultValue = defaultValue
+    //主URL拉取失败，尝试拉取备份URL，MD5检查、是否使用本地值可自定义
+    fun get(configValue: String, defaultValue: String, forceCheckMD5: Boolean, useCache: Boolean, callback: ResponseHandler) {
+        this.fetchURLWithMD5 = configValue
+        this.backupURLWithMD5 = defaultValue
         this.forceCheckMD5 = forceCheckMD5
-        this.saveDataToLocal = saveToLocal
+        this.saveDataToLocal = useCache
         execute(callback)
     }
 
     private fun execute(callback: ResponseHandler) {
-        var configValueArray = configValue.split(";".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        var configValueArray = fetchURLWithMD5.split(";".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
         val configUrl = ConvertUtils.getSafeValueFromArray(configValueArray, 0, "")
         val configMd5 = ConvertUtils.getSafeValueFromArray(configValueArray, 1, "")
 
@@ -99,19 +121,19 @@ class ZixieRequestFile {
     }
 
     private fun loadDefaultValue(callback: ResponseHandler) {
-        var defaultValueArray = defaultValue.split(";".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        var defaultValueArray = backupURLWithMD5.split(";".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
         val defaultUrl = ConvertUtils.getSafeValueFromArray(defaultValueArray, 0, "")
         val defaultMd5 = ConvertUtils.getSafeValueFromArray(defaultValueArray, 1, "")
 
         if (defaultUrl.isNullOrEmpty()) {
             // 所有的URL 配置都错误
             ZLog.e(TAG, "default config error, url is empty")
-            callback.onFailed(ResponseHandler.ERROR_CONFIG, "default config error, url is empty: $configValue; $defaultValue")
+            callback.onFailed(ResponseHandler.ERROR_CONFIG, "default config error, url is empty: $fetchURLWithMD5; $backupURLWithMD5")
         } else {
             // MD5 无效且校验MD5，拉取默认数据
             if (defaultMd5.isNullOrEmpty() && forceCheckMD5) {
                 ZLog.e(TAG, "default config error, md5 is empty")
-                callback.onFailed(ResponseHandler.ERROR_CONFIG, "default config error, md5 is empty: $configValue; $defaultValue")
+                callback.onFailed(ResponseHandler.ERROR_CONFIG, "default config error, md5 is empty: $fetchURLWithMD5; $backupURLWithMD5")
             } else {
                 loadAndSaveData(defaultUrl, defaultMd5, true, object : ResponseHandler {
                     override fun onSuccess(type: Int, response: String) {
