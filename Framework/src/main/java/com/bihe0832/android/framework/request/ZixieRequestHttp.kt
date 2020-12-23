6dd68fbdf3b54099802e62e455ef2b81baee61d3
子勰 <code@bihe0832.com>
2020-12-23 14:42:33 +0800

    文件直接下载

---
+++ Framework/src/main/java/com/bihe0832/android/framework/request/ZixieRequestHttp.kt
@@ -0,0 +1,52 @@
+package com.bihe0832.android.framework.request
+
+
+import com.bihe0832.android.lib.http.common.HTTPServer
+import com.bihe0832.android.lib.http.common.HttpBasicRequest
+import com.bihe0832.android.lib.http.common.HttpResponseHandler
+
+/**
+ * @author hardyshi code@bihe0832.com
+ * Created on 2019-08-01.
+ * Description: Description
+ */
+object ZixieRequestHttp {
+
+    fun get(url: String): String {
+        return HTTPServer.getInstance().doRequestAsync(url)
+    }
+
+    fun post(url: String, params: String): String {
+        return HTTPServer.getInstance().doRequestAsync(url, params)
+    }
+
+    fun get(url: String, responseHandler: HttpResponseHandler) {
+        object : HttpBasicRequest() {
+            override fun getUrl(): String {
+                return url
+            }
+
+            override fun getResponseHandler(): HttpResponseHandler {
+                return responseHandler
+            }
+        }.let {
+            HTTPServer.getInstance().doRequest(it)
+        }
+    }
+
+    fun post(url: String, postData: ByteArray, responseHandler: HttpResponseHandler) {
+        object : HttpBasicRequest() {
+            override fun getUrl(): String {
+                return url
+            }
+
+            override fun getResponseHandler(): HttpResponseHandler {
+                return responseHandler
+            }
+        }.apply {
+            data = postData
+        }.let {
+            HTTPServer.getInstance().doRequest(it)
+        }
+    }
+}
