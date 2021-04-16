4d30d85197a871e47e9b94332a651b5ac86113b7
子勰 <code@bihe0832.com>
2021-04-16 21:52:30 +0800

    MainFragment

--- Framework/src/main/java/com/bihe0832/android/framework/ui/main/CommonMainFragment.java
+++ Framework/src/main/java/com/bihe0832/android/framework/ui/main/CommonMainFragment.java
@@ -5,23 +5,21 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.bihe0832.android.framework.R;
 import com.bihe0832.android.framework.ui.BaseFragment;
 import com.bihe0832.android.lib.log.ZLog;
 import com.bihe0832.android.lib.ui.view.BottomBar;
 import com.bihe0832.android.lib.ui.view.BottomBarTab;
-
 import java.util.ArrayList;
 
 /**
  * @author hardyshi code@bihe0832.com
- * Created on 2020/8/3.
- * Description: Description
+ *         Created on 2020/8/3.
+ *         Description: Description
  */
 public abstract class CommonMainFragment extends BaseFragment {
 
-    private BottomBar bottomBar = null;
+    protected BottomBar bottomBar = null;
     private int currentPosition = getDefaultTabID();
     private ArrayList<BaseFragment> mFragments = new ArrayList<>();
     private ArrayList<BottomBarTab> mBottomBarTabs = new ArrayList<>();
@@ -36,7 +34,8 @@ public abstract class CommonMainFragment extends BaseFragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.common_fragment_with_bottom, container, false);
         return view;
     }
@@ -76,12 +75,21 @@ public abstract class CommonMainFragment extends BaseFragment {
         bottomBar.setCurrentItem(getDefaultTabID());
     }
 
+    protected void changeTab(final int position) {
+        if (position < bottomBar.getChildCount()) {
+            bottomBar.setCurrentItem(position);
+        }
+    }
+
+    protected BottomBar getBottomBar() {
+        return bottomBar;
+    }
 
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
         ZLog.d("setUserVisibleHint:$isVisibleToUser");
-        if(mFragments.size() > currentPosition && null != mFragments.get(currentPosition)){
+        if (mFragments.size() > currentPosition && null != mFragments.get(currentPosition)) {
             mFragments.get(currentPosition).setUserVisibleHint(isVisibleToUser);
         }
     }
