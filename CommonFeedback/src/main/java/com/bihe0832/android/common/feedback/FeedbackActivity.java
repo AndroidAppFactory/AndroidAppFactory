5d8c8fa6c4fc42f4604fd9ebe3eb009a86e21822
子勰 <code@bihe0832.com>
2021-06-07 22:57:39 +0800

    修复webview 重载误用导致自动刷新

--- CommonFeedback/src/main/java/com/bihe0832/android/common/feedback/FeedbackActivity.java
+++ CommonFeedback/src/main/java/com/bihe0832/android/common/feedback/FeedbackActivity.java
@@ -1,47 +1,40 @@
 package com.bihe0832.android.common.feedback;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
+import android.text.TextUtils;
 
-import com.bihe0832.android.framework.ZixieContext;
-import com.bihe0832.android.framework.ui.main.CommonActivity;
+import com.bihe0832.android.common.webview.BaseWebviewActivity;
 import com.bihe0832.android.common.webview.BaseWebviewFragment;
+import com.bihe0832.android.framework.ZixieContext;
 import com.bihe0832.android.lib.log.ZLog;
 import com.bihe0832.android.lib.router.annotation.Module;
 
-import java.net.URLDecoder;
-
 
 @Module(FeedbackActivity.MODULE_NAME_FEEDBACK)
-public class FeedbackActivity extends CommonActivity {
+public final class FeedbackActivity extends BaseWebviewActivity {
 
     public static final String MODULE_NAME_FEEDBACK = "feedback";
 
-    private String mURL = "";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected BaseWebviewFragment getWebViewFragment() {
+        return FeedbackFragment.newInstance(mURL);
+    }
+
+    @Override
+    protected Class getWebViewFragmentClass() {
+        return FeedbackFragment.class;
+    }
+
+    @Override
+    protected void initToolbar() {
         initToolbar("建议反馈", true);
-        handleIntent(getIntent());
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        if (findFragment(FeedbackFragment.class) == null) {
-            loadRootFragment(R.id.common_fragment_content, FeedbackFragment.newInstance(mURL));
-        } else {
-            finish();
-        }
-    }
-
-    private void handleIntent(Intent intent) {
-        if (intent.hasExtra(BaseWebviewFragment.INTENT_KEY_URL)) {
-            mURL = URLDecoder.decode(intent.getStringExtra(BaseWebviewFragment.INTENT_KEY_URL));
-        } else {
-            ZLog.e("handle intent, but extra is bad");
+    protected void handleIntent(Intent intent) {
+        super.handleIntent(intent);
+        if (TextUtils.isEmpty(mURL)) {
             ZixieContext.INSTANCE.showToast("请输入正确的反馈地址");
             finish();
             return;
