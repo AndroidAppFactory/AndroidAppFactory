ca3a79fe2af9c936459f6168e4311e4b7a00c865
子勰 <code@bihe0832.com>
2021-06-25 14:38:01 +0800

    日志模块优化

---
+++ BaseTest/src/main/java/com/bihe0832/android/base/test/log/TestLogFragment.kt
@@ -0,0 +1,55 @@
+package com.bihe0832.android.base.test.log
+
+
+import android.view.View
+import com.bihe0832.android.common.test.base.BaseTestFragment
+import com.bihe0832.android.common.test.item.TestItemData
+import com.bihe0832.android.framework.log.LoggerFile
+import com.bihe0832.android.framework.log.LoggerTrace
+import com.bihe0832.android.lib.adapter.CardBaseModule
+import com.bihe0832.android.lib.log.ZLog
+import com.bihe0832.android.lib.thread.ThreadManager
+
+class TestLogFragment : BaseTestFragment() {
+    val LOG_TAG = "TestLogFragment"
+
+    override fun getDataList(): ArrayList<CardBaseModule> {
+        return ArrayList<CardBaseModule>().apply {
+            add(TestItemData("简单日志", View.OnClickListener { ZLog.d("test") }))
+            add(TestItemData("耗时打点", View.OnClickListener { testTrace() }))
+            add(TestItemData("文件日志", View.OnClickListener { testLogFile() }))
+            add(TestItemData("打开文件日志", View.OnClickListener { LoggerFile.openLog(LOG_TAG) }))
+            add(TestItemData("发送文件日志", View.OnClickListener { LoggerFile.sendLog(LOG_TAG) }))
+            add(TestItemData("跨线程文件日志", View.OnClickListener { testLog() }))
+        }
+    }
+
+    private fun testTrace() {
+        LoggerTrace.tracePoint("Test 2")
+        LoggerTrace.tracePoint(LOG_TAG, "Test 1")
+        LoggerTrace.tracePoint(LOG_TAG, "Test 3", needLog = false, needDuration = false, needFile = false)
+        LoggerTrace.tracePoint(LOG_TAG, "Test 4", needLog = false, needDuration = true, needFile = false)
+        LoggerTrace.tracePoint(LOG_TAG, "Test 5", true, needDuration = false, needFile = false)
+        LoggerTrace.tracePoint(LOG_TAG, "Test 5", true, needDuration = true, needFile = true)
+        for (i in 0..20) {
+            ThreadManager.getInstance().start({
+                LoggerTrace.tracePoint(LOG_TAG, "Test $i")
+            }, i * 333L + i)
+        }
+        LoggerTrace.showResult()
+        LoggerTrace.showResult(LOG_TAG)
+    }
+
+    private fun testLogFile() {
+        LoggerFile.log(LOG_TAG, "test ")
+    }
+
+    private fun testLog() {
+        for (i in 0..20) {
+            ThreadManager.getInstance().start({
+                LoggerFile.log(LOG_TAG, "test by auto  $i")
+            }, i * 22L + i)
+        }
+    }
+
+}
