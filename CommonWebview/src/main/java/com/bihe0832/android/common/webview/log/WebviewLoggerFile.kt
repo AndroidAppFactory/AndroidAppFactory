a178a5e442584cd94b05969b074146e69389afbe
子勰 <code@bihe0832.com>
2021-07-12 20:05:10 +0800

    增加webview的日志并优化日志获取

---
+++ CommonWebview/src/main/java/com/bihe0832/android/common/webview/log/WebviewLoggerFile.kt
@@ -0,0 +1,25 @@
+package com.bihe0832.android.common.webview.log
+
+import com.bihe0832.android.framework.log.LoggerFile
+
+
+/**
+ *
+ * @author hardyshi code@bihe0832.com
+ * Created on 2020-03-05.
+ * Description: 用户处理特殊日志
+ *
+ */
+object WebviewLoggerFile {
+
+    const val MODULE_NAME = "webview"
+
+
+    fun log(msg: String) {
+        LoggerFile.log(LoggerFile.getZixieFileLogPathByModule(MODULE_NAME), msg)
+    }
+
+    fun getWebviewLogPath(): String {
+        return LoggerFile.getZixieFileLogPathByModule(MODULE_NAME);
+    }
+}
