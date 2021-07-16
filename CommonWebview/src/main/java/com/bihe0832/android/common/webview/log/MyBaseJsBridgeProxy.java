b8458ca23e894a2e8b8113af037768dca8c17610
子勰 <code@bihe0832.com>
2021-07-16 12:26:51 +0800

    完善webview 日志，内置路由调整名称

---
+++ CommonWebview/src/main/java/com/bihe0832/android/common/webview/log/MyBaseJsBridgeProxy.java
@@ -0,0 +1,33 @@
+package com.bihe0832.android.common.webview.log;
+
+import android.app.Activity;
+import android.net.Uri;
+import com.bihe0832.android.lib.webview.jsbridge.BaseJsBridgeProxy;
+import com.bihe0832.android.lib.webview.jsbridge.JsBridge;
+import com.tencent.smtt.sdk.WebView;
+import java.net.URLDecoder;
+
+/**
+ * @author hardyshi code@bihe0832.com Created on 7/16/21.
+ */
+public class MyBaseJsBridgeProxy extends BaseJsBridgeProxy {
+
+    public MyBaseJsBridgeProxy(WebView webView, Activity activity) {
+        super(webView, activity, new MyJSBridge(webView, activity));
+    }
+
+    public MyBaseJsBridgeProxy(WebView webView, Activity activity, JsBridge jsBridge) {
+        super(webView, activity, jsBridge);
+    }
+
+    @Override
+    protected void callAMethod(Uri uri, String hostAsMethodName, int seqid, String callbackName) {
+        WebviewLoggerFile.INSTANCE.log("---------------------- JsBridge call start ----------------------");
+        WebviewLoggerFile.INSTANCE.log("uri:" + URLDecoder.decode(uri.toString()));
+        WebviewLoggerFile.INSTANCE.log("hostAsMethodName:" + hostAsMethodName);
+        WebviewLoggerFile.INSTANCE.log("seqid:" + seqid);
+        WebviewLoggerFile.INSTANCE.log("callbackName:" + callbackName);
+        WebviewLoggerFile.INSTANCE.log("---------------------- JsBridge call end ----------------------");
+        super.callAMethod(uri, hostAsMethodName, seqid, callbackName);
+    }
+}
