ec201ca3e16de67c6a6e93f1242e1a9237329863
子勰 <code@bihe0832.com>
2021-08-13 14:03:30 +0800

    优化部分机型获取日志目录异常

--- APPTest/src/main/java/com/bihe0832/android/test/TestMainActivity.kt
+++ APPTest/src/main/java/com/bihe0832/android/test/TestMainActivity.kt
@@ -70,9 +70,9 @@ class TestMainActivity : CommonActivity() {
 
     override fun getPermissionList(): List<String> {
         return ArrayList<String>().apply {
-//            add(Manifest.permission.CAMERA)
+            add(Manifest.permission.CAMERA)
 //            add(Manifest.permission.RECORD_AUDIO)
-//            add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
 //            add(Manifest.permission.SYSTEM_ALERT_WINDOW)
         }
     }
