5a7db533c313beb48a6e2da5bc70323c66b92087
子勰 <code@bihe0832.com>
2021-12-17 17:47:44 +0800

    路由优化

--- CommonTest/src/main/java/com/bihe0832/android/common/test/module/TestDebugRouterFragment.kt
+++ CommonTest/src/main/java/com/bihe0832/android/common/test/module/TestDebugRouterFragment.kt
@@ -1,5 +1,6 @@
 package com.bihe0832.android.common.test.module
 
+import android.content.Intent
 import android.view.View
 import com.bihe0832.android.common.test.base.BaseTestListFragment
 import com.bihe0832.android.common.test.item.TestItemData
@@ -10,20 +11,24 @@ import com.bihe0832.android.lib.router.Routers
 open class TestDebugRouterFragment : BaseTestListFragment() {
 
     inner class RouterItem(url: String) : TestItemData(
-            url,
-            View.OnClickListener { Routers.open(context, url) },
-            View.OnLongClickListener {
-                showInfo("复制并分享路由地址", url)
-                true
-            }
+        url,
+        View.OnClickListener { Routers.open(context, url) },
+        View.OnLongClickListener {
+            showInfo("复制并分享路由地址", url)
+            true
+        }
     )
 
     final override fun getDataList(): ArrayList<CardBaseModule> {
         return ArrayList<CardBaseModule>().apply {
-            add(TestTipsData(
+            add(
+                TestTipsData(
                     "可以在PC打开下面链接生成二维码后测试：<small>https://microdemo.bihe0832.com/MyJS/router/</small> ",
                     View.OnClickListener {
-                        showInfo("路由测试工具链接分享", "路由测试工具链接：\n https://microdemo.bihe0832.com/MyJS/router/")
+                        showInfo(
+                            "路由测试工具链接分享",
+                            "路由测试工具链接：\n https://microdemo.bihe0832.com/MyJS/router/"
+                        )
                     })
             )
 
