9a642bb0e0423081bd03c17644cd25d4d47cce6b
子勰 <code@bihe0832.com>
2022-07-29 15:23:22 +0800

    sqlite 支持sql

--- LibSqlite/src/main/java/com/bihe0832/android/lib/sqlite/BaseDBHelper.java
+++ LibSqlite/src/main/java/com/bihe0832/android/lib/sqlite/BaseDBHelper.java
@@ -6,75 +6,99 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.bihe0832.android.lib.log.ZLog;
+
 public abstract class BaseDBHelper extends SQLiteOpenHelper {
-	public BaseDBHelper(Context ctx, String name, int version) {
-		super (ctx, name, null, version);
-	}
-	
+
+    public static final String TAG = "SQLiteOpenHelper";
+
+    public BaseDBHelper(Context ctx, String name, int version) {
+        super(ctx, name, null, version);
+    }
+
     public synchronized long insert(String table, String nullColumnHack, ContentValues values) {
-		SQLiteDatabase db = null;
-		long rowid = -1;
+        SQLiteDatabase db = null;
+        long rowid = -1;
 
-		try {
-			db = getWritableDatabase();
-			rowid = db.insert(table, nullColumnHack, values);
-		} catch (Exception e) {
-			e.printStackTrace();
-			rowid = -1;
-		}
+        try {
+            db = getWritableDatabase();
+            rowid = db.insert(table, nullColumnHack, values);
+        } catch (Exception e) {
+            e.printStackTrace();
+            rowid = -1;
+        }
 
-		return rowid;
+        return rowid;
     }
 
     public synchronized int delete(String table, String whereClause, String[] whereArgs) {
-		SQLiteDatabase db = null;
-		int rows = 0;
-		
-		try {
-			db = getWritableDatabase();
-			rows = db.delete(table, whereClause, whereArgs);
-		} catch (Exception e) {
-			e.printStackTrace();
-			rows = 0;
-		}
+        SQLiteDatabase db = null;
+        int rows = 0;
 
-		return rows;
+        try {
+            db = getWritableDatabase();
+            rows = db.delete(table, whereClause, whereArgs);
+        } catch (Exception e) {
+            e.printStackTrace();
+            rows = 0;
+        }
+
+        return rows;
     }
 
     public synchronized int update(String table, ContentValues values, String whereClause,
-            String[] whereArgs) {
-		SQLiteDatabase db = null;
-		int rows = 0;
+                                   String[] whereArgs) {
+        SQLiteDatabase db = null;
+        int rows = 0;
 
-		try {
-			db = getWritableDatabase();
-			rows = db.update(table, values, whereClause, whereArgs);
-		} catch (Exception e) {
-			e.printStackTrace();
-			rows = 0;
-		}
+        try {
+            db = getWritableDatabase();
+            rows = db.update(table, values, whereClause, whereArgs);
+        } catch (Exception e) {
+            e.printStackTrace();
+            rows = 0;
+        }
 
-		return rows;
+        return rows;
     }
 
     public synchronized Cursor queryInfo(String table, String[] columns, String selection,
-            String[] selectionArgs, String groupBy, String having,
-            String orderBy, String limit) {
-		SQLiteDatabase db = null;
-		Cursor cursor = null;
+                                         String[] selectionArgs, String groupBy, String having,
+                                         String orderBy, String limit) {
+        SQLiteDatabase db = null;
+        Cursor cursor = null;
 
-		try {
-			db = getReadableDatabase();
-			cursor = db.query(table, columns, selection, selectionArgs,
-					groupBy, having, orderBy, limit);
-		} catch (Exception e) {
-			e.printStackTrace();
-			if (cursor != null) {
-				cursor.close();
-			}
-			cursor = null;
-		}
+        try {
+            db = getReadableDatabase();
+            cursor = db.query(table, columns, selection, selectionArgs,
+                    groupBy, having, orderBy, limit);
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (cursor != null) {
+                cursor.close();
+            }
+            cursor = null;
+        }
 
-		return cursor;
-	}
+        return cursor;
+    }
+
+    public synchronized Cursor queryInfo(String sql) {
+        SQLiteDatabase db = null;
+        Cursor cursor = null;
+
+        try {
+            ZLog.d(TAG, sql);
+            db = getReadableDatabase();
+            cursor = db.rawQuery(sql, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (cursor != null) {
+                cursor.close();
+            }
+            cursor = null;
+        }
+
+        return cursor;
+    }
 }
