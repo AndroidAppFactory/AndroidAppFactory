0368047224e92abb38453a990d136592aa7c667c
子勰 <code@bihe0832.com>
2022-09-08 17:37:37 +0800

    协程调用优化

--- CommonCoroutines/src/main/java/com/bihe0832/android/common/coroutines/ZixieCoroutinesData.kt
+++ CommonCoroutines/src/main/java/com/bihe0832/android/common/coroutines/ZixieCoroutinesData.kt
@@ -1,35 +1,56 @@
 package com.bihe0832.android.common.coroutines
 
+import kotlin.math.E
+
 /**
  * @author zixie code@bihe0832.com Created on 2022/2/10.
  */
 
 open class ZixieCoroutinesData<T> {
 
-    data class Error(val errCode: Int, val msg: String?, val exception: Exception?)
+    data class Error(val flag: Int, val errCode: Int, val msg: String?, val exception: Exception?)
 
     private var SUCCESS: T? = null
-    private var ERROR: Error? = null
+    protected var ERROR: Error? = null
 
     constructor(successData: T) {
         SUCCESS = successData
     }
 
     constructor(errorCode: Int, msg: String?) {
-        ERROR = Error(errorCode, msg, null)
+        ERROR = Error(0, errorCode, msg, null)
     }
 
     constructor(errorCode: Int, exception: Exception?) {
-        ERROR = Error(errorCode, "", exception)
+        ERROR = Error(0, errorCode, "", exception)
     }
 
     constructor(errorCode: Int, msg: String?, exception: Exception?) {
-        ERROR = Error(errorCode, msg, exception)
+        ERROR = Error(0, errorCode, msg, exception)
     }
 
-    fun onSuccess(action: (success: T) -> Unit) {
+    constructor(type: Int, errorCode: Int, msg: String?, exception: Exception?) {
+        ERROR = Error(type, errorCode, msg, exception)
+    }
+
+    fun onSuccess(action: (success: T) -> Unit): ZixieCoroutinesData<T> {
         SUCCESS?.let {
             action(it)
+        }
+        return this
+    }
+
+    fun onSuccess(): T? {
+        return SUCCESS
+    }
+
+    fun onError(): Error? {
+        return ERROR
+    }
+
+    protected fun onInnerError(action: (flag: Int, errCode: Int, msg: String?, exception: Exception?) -> Unit) {
+        ERROR?.let {
+            action(it.flag, it.errCode, it.msg, it.exception)
         }
     }
 
@@ -38,5 +59,4 @@ open class ZixieCoroutinesData<T> {
             action(it.errCode, it.msg, it.exception)
         }
     }
-
 }
