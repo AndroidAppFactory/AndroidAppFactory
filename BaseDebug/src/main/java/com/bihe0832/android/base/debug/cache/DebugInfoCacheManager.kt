9753a456f1905d6f4f8ec1c9321c9787426ed514
子勰 <code@bihe0832.com>
2022-09-08 16:14:19 +0800

    协程调用优化

--- BaseDebug/src/main/java/com/bihe0832/android/base/debug/cache/DebugInfoCacheManager.kt
+++ BaseDebug/src/main/java/com/bihe0832/android/base/debug/cache/DebugInfoCacheManager.kt
@@ -1,6 +1,5 @@
 package com.bihe0832.android.base.debug.cache
 
-import com.bihe0832.android.common.coroutines.ZixieCoroutinesException
 import com.bihe0832.android.common.data.center.InfoCacheManager
 import com.bihe0832.android.lib.aaf.tools.AAFDataCallback
 import com.bihe0832.android.lib.log.ZLog
@@ -16,8 +15,8 @@ import kotlinx.coroutines.launch
 object DebugInfoCacheManager {
     private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
 
-    private val mTestInfoCacheManagerImpl = object : InfoCacheManager<Any?>() {
-        override fun getRemoteData(key: String, listener: AAFDataCallback<Any?>) {
+    private val mTestInfoCacheManagerImpl = object : InfoCacheManager<DebugCacheData>() {
+        override fun getRemoteData(key: String, listener: AAFDataCallback<DebugCacheData>) {
             DebugCacheData().apply {
                 this.key = key
             }.let {
@@ -39,17 +38,16 @@ object DebugInfoCacheManager {
         } else if (key < 0) {
             add = true
         }
-
-        try {
-            CoroutineScope(defaultDispatcher).launch {
-                mTestInfoCacheManagerImpl.getData(key.toString(), 15000L).let {
+        CoroutineScope(defaultDispatcher).launch {
+            mTestInfoCacheManagerImpl.getData(key.toString(), 15000L).let {
+                it.onSuccess {
                     ZLog.d("zixie", it.toString())
                 }
-            }
-        } catch (e: ZixieCoroutinesException) {
-            e.printStackTrace()
-            ZLog.d("zixie", e.toString())
-        }
 
+                it.onError { errCode, msg, exception ->
+                    ZLog.d("zixie", "$errCode, $msg, $exception")
+                }
+            }
+        }
     }
 }
