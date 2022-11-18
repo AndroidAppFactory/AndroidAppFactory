4638a7c2a6e5e1eb26a3a19d092e1b7b5bd43119
子勰 <code@bihe0832.com>
2022-11-18 16:18:58 +0800

    data center 增加自动清理策略

--- LibDataCenter/src/main/java/com/bihe0832/android/common/data/center/InfoCacheManager.kt
+++ LibDataCenter/src/main/java/com/bihe0832/android/common/data/center/InfoCacheManager.kt
@@ -18,55 +18,57 @@ abstract class InfoCacheManager<T> {
 
     val TAG = "InfoCacheManager"
 
+    // 默认的cache 数据缓存时间
+    val DEFAULT_DURATION = 1000 * 60 * 30L
+
+    // cache 满了以后，单次清除的内容量
+    val DEFAULT_PART = 20
+
     abstract fun getRemoteData(key: String, listener: AAFDataCallback<T>)
 
-    private val mDataList: ConcurrentHashMap<String, InfoItem<T>> = ConcurrentHashMap()
+    open fun getDefaultDuration(): Long {
+        return DEFAULT_DURATION
+    }
+
+    open fun getBestLength(): Int {
+        return 3000
+    }
+
+    private val mDataMap: ConcurrentHashMap<String, InfoItem<T>> = ConcurrentHashMap()
 
     inner class InfoItem<T>(
+            //数据key
             var key: String = "",
+            //数据插入或者更新时间
             var updateTime: Long = 0L,
-            var dataItem: T? = null
-    )
+            // 数据内容
+            var dataItem: T? = null)
 
-    inner class ContinuationCallbackForFetchDataListener<T>(private var continuation: Continuation<ZixieCoroutinesData<T>>) :
-            AAFDataCallback<T>() {
+    inner class ContinuationCallbackForFetchDataListener<T>(private var continuation: Continuation<ZixieCoroutinesData<T>>) : AAFDataCallback<T>() {
 
         override fun onSuccess(result: T?) {
             if (result != null) {
                 continuation.resume(ZixieCoroutinesData(result))
             } else {
-                continuation.resume(
-                        ZixieCoroutinesData(
-                                -1,
-                                Coroutines_ERROR_DATA_NULL,
-                                ""
-                        )
-                )
+                continuation.resume(ZixieCoroutinesData(-1, Coroutines_ERROR_DATA_NULL, ""))
             }
         }
 
         override fun onError(code: Int, msg: String) {
-            continuation.resume(
-                    ZixieCoroutinesData(
-                            code,
-                            code,
-                            msg
-                    )
-            )
+            continuation.resume(ZixieCoroutinesData(code, code, msg))
         }
     }
 
-
-    open suspend fun getData(key: String): ZixieCoroutinesData<T> = getData(key, -1)
-
-    open suspend fun getData(key: String, duration: Long): ZixieCoroutinesData<T> =
-            suspendCoroutine { cont ->
-                getData(key, duration, ContinuationCallbackForFetchDataListener(cont))
+    private fun getRealBestLength(): Int {
+        getBestLength().let {
+            if (it > DEFAULT_PART) {
+                return it
+            } else {
+                return DEFAULT_PART
             }
-
-    fun getData(key: String, listener: AAFDataCallback<T>) {
-        getData(key, -1, listener)
+        }
     }
+
 
     fun forceUpdate(key: String, forceResetWhenFailed: Boolean) {
         getData(key, -1, object : AAFDataCallback<T>() {
@@ -77,18 +79,53 @@ abstract class InfoCacheManager<T> {
             override fun onError(errorCode: Int, msg: String) {
                 super.onError(errorCode, msg)
                 if (forceResetWhenFailed) {
-                    resetData(key)
+                    removeData(key)
                 }
             }
         })
     }
 
-    fun resetData(key: String) {
-        mDataList.remove(key)
+    fun removeData(key: String) {
+        mDataMap.remove(key)
     }
 
     fun addData(key: String, data: T) {
-        mDataList.put(key, InfoItem(key, System.currentTimeMillis(), data))
+        ZLog.d(TAG, "add data：$key ")
+        mDataMap[key] = InfoItem(key, System.currentTimeMillis(), data)
+        if (mDataMap.size > getRealBestLength()) {
+            var dataIterator = mDataMap.entries.iterator()
+            var num = 0
+            var startCheckTime = System.currentTimeMillis()
+            while (dataIterator.hasNext() && num < getRealBestLength() / DEFAULT_PART) {
+                dataIterator.next().let {
+                    var time = it.value.updateTime
+                    if (time < startCheckTime - getDefaultDuration()) {
+                        ZLog.d(TAG, "remove ${it.key} data by length")
+                        dataIterator.remove()
+                        num++
+                    }
+                }
+            }
+            ZLog.d(TAG, "after remove data length is：${mDataMap.size} ")
+        }
+    }
+
+
+    open suspend fun getNewData(key: String): ZixieCoroutinesData<T> = getData(key, -1)
+
+    open suspend fun getData(key: String, duration: Long): ZixieCoroutinesData<T> = suspendCoroutine { cont ->
+        getData(key, duration, ContinuationCallbackForFetchDataListener(cont))
+    }
+
+    /**
+     * 彻底不看过期时间获取数据
+     */
+    open suspend fun getCachedData(key: String): ZixieCoroutinesData<T> {
+        return getData(key, getDefaultDuration())
+    }
+
+    fun getNewData(key: String, listener: AAFDataCallback<T>) {
+        getData(key, -1, listener)
     }
 
     fun getData(key: String, duration: Long, listener: AAFDataCallback<T>?) {
@@ -108,8 +145,8 @@ abstract class InfoCacheManager<T> {
 
         }
 
-        if (mDataList.containsKey(key)) {
-            mDataList[key].let {
+        if (mDataMap.containsKey(key)) {
+            mDataMap[key].let {
                 if (null != it) {
                     if (null != it.dataItem && duration > 0 && System.currentTimeMillis() - duration < it.updateTime) {
                         ZLog.d(TAG, "read $key data from cache")
@@ -126,7 +163,19 @@ abstract class InfoCacheManager<T> {
         }
     }
 
-    fun getCachedData(key: String): InfoItem<T>? {
-        return mDataList.get(key)
+    /**
+     * 彻底不看过期时间获取数据
+     */
+    fun getCachedData(key: String, listener: AAFDataCallback<T>?) {
+        return getData(key, getDefaultDuration(), listener)
     }
+
+    /**
+     * 彻底不看过期时间获取数据
+     */
+    fun getSourceData(key: String): InfoItem<T>? {
+        return mDataMap.get(key)
+    }
+
+
 }
