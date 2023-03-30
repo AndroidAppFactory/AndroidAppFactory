6804c165aa5a761085c75a85b4572a65c34587bd
子勰 <code@bihe0832.com>
2023-03-30 10:30:03 +0800

    电量获取优化

---
+++ LibDeviceNetwork/src/main/java/com/bihe0832/android/lib/device/battery/BatteryHelper.kt
@@ -0,0 +1,48 @@
+package com.bihe0832.android.lib.device.battery
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+
+/**
+ *
+ * @author hardyshi code@bihe0832.com
+ * Created on 2023/3/30.
+ * Description: Description
+ *
+ */
+
+object BatteryHelper {
+
+    fun startReceiveBatteryChanged(context: Context?, statusChangeReceiver: BroadcastReceiver) {
+        val powerConnectedFilter = IntentFilter(Intent.ACTION_POWER_CONNECTED)
+        powerConnectedFilter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+        powerConnectedFilter.addAction(Intent.ACTION_BATTERY_LOW)
+        powerConnectedFilter.addAction(Intent.ACTION_BATTERY_OKAY)
+        context?.registerReceiver(statusChangeReceiver, powerConnectedFilter)
+    }
+
+    fun stopReceiveBatteryChanged(context: Context?, statusChangeReceiver: BroadcastReceiver) {
+        context?.unregisterReceiver(statusChangeReceiver)
+    }
+
+    fun getBatteryStatus(context: Context?): BatteryStatus? {
+        context?.registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))?.let { batteryStatus ->
+            // Are we charging / charged?
+            val status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1)
+            val isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING || status == BatteryManager.BATTERY_STATUS_FULL
+
+            // How are we charging?
+            val chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1)
+
+            // Get the current battery percentage
+            val level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+            val scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+
+            return BatteryStatus(isCharging, chargePlug, level, scale)
+        }
+        return null
+    }
+}
---
+++ LibDeviceNetwork/src/main/java/com/bihe0832/android/lib/device/battery/BatteryStatus.kt
@@ -0,0 +1,43 @@
+package com.bihe0832.android.lib.device.battery
+
+import android.os.BatteryManager
+import com.bihe0832.android.lib.utils.MathUtils
+
+/**
+ *
+ * @author hardyshi code@bihe0832.com
+ * Created on 2023/3/30.
+ * Description: Description
+ *
+ */
+data class BatteryStatus(
+        // 当前是否在充电
+        val isCharging: Boolean,
+        // 当前充电类型
+        val plugged: Int,
+        // 当前电量的相对值
+        val currentValue: Int,
+        // 电量的基准值
+        val scale: Int) {
+    override fun toString(): String {
+        return "BatteryStatus(isCharging=$isCharging, plugged=$plugged, batteryPct=$currentValue)"
+    }
+
+    fun getBatteryPercentDesc(): String {
+        return MathUtils.getFormatPercentDesc(getBatteryPercent())
+    }
+
+    fun getBatteryPercent(): Float {
+        return MathUtils.getFormatPercent(currentValue, scale, 4)
+    }
+
+    fun getChargeTypeDesc(): String {
+        return when (plugged) {
+            BatteryManager.BATTERY_PLUGGED_AC -> "AC"
+            BatteryManager.BATTERY_PLUGGED_USB -> "USB"
+            BatteryManager.BATTERY_PLUGGED_WIRELESS -> "WIRELESS"
+            else -> "UNKNOWN"
+        }
+    }
+
+}
