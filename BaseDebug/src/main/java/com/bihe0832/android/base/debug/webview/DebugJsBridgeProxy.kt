2e84c9049a98f1bbce3ad21ed71fedf6c3a50f0a
子勰 <code@bihe0832.com>
2023-05-31 18:25:45 +0800

    H5 内嵌原生demo 完善

---
+++ BaseDebug/src/main/java/com/bihe0832/android/base/debug/webview/DebugJsBridgeProxy.kt
@@ -0,0 +1,70 @@
+package com.bihe0832.android.base.debug.webview
+
+import android.app.Activity
+import android.net.Uri
+import android.view.View
+import android.widget.AbsoluteLayout
+import com.bihe0832.android.common.webview.log.MyBaseJsBridgeProxy
+import com.bihe0832.android.framework.ZixieContext.applicationContext
+import com.bihe0832.android.lib.thread.ThreadManager
+import com.bihe0832.android.lib.utils.ConvertUtils
+import com.bihe0832.android.lib.utils.os.DisplayUtil
+import com.bihe0832.android.lib.webview.BaseWebView
+import com.bihe0832.android.lib.webview.jsbridge.JsResult
+
+/**
+ * Created by hardyshi on 2016/10/20.
+ */
+class DebugJsBridgeProxy(webView: BaseWebView, activity: Activity) : MyBaseJsBridgeProxy(webView, activity) {
+
+    private var mNativeView: View? = null
+
+    fun setNativeView(mNativeView: View?) {
+        this.mNativeView = mNativeView
+    }
+
+    fun showNativeView(uri: Uri?, seqid: Int, method: String?, callbackFun: String?) {
+        try {
+            if (null != mNativeView) {
+                ThreadManager.getInstance().runOnUIThread {
+                    mNativeView!!.visibility = View.VISIBLE
+                    resetNativeViewHeight(mNativeView!!.height)
+                    mJsBridge.response(callbackFun, seqid, method, "showNativeView success")
+                }
+            } else {
+                mJsBridge.responseFail(callbackFun, seqid, method, JsResult.Code_Java_Exception)
+            }
+        } catch (var11: Exception) {
+            mJsBridge.responseFail(callbackFun, seqid, method, JsResult.Code_Java_Exception)
+        }
+    }
+
+    fun hideNativeView(uri: Uri?, seqid: Int, method: String?, callbackFun: String?) {
+        try {
+            if (null != mNativeView) {
+                ThreadManager.getInstance().runOnUIThread {
+                    mNativeView!!.visibility = View.GONE
+                    resetNativeViewHeight(0)
+                    mJsBridge.response(callbackFun, seqid, method, null)
+                }
+            } else {
+                mJsBridge.responseFail(callbackFun, seqid, method, JsResult.Code_Java_Exception)
+            }
+        } catch (var11: Exception) {
+            mJsBridge.responseFail(callbackFun, seqid, method, JsResult.Code_Java_Exception)
+        }
+    }
+
+    private fun resetNativeViewHeight(height: Int) {
+        webView.evaluateJavascript("javaScript:getNativeViewPosition()") { value ->
+            ThreadManager.getInstance().runOnUIThread {
+                val top = ConvertUtils.parseFloat(value, -1f)
+                val params = mNativeView!!.layoutParams as AbsoluteLayout.LayoutParams
+                params.y = DisplayUtil.dip2pxWithDefaultDensity(applicationContext, top)
+                mNativeView!!.layoutParams = params
+                webView.loadUrl("javaScript:setNativeViewHeight(" + DisplayUtil.px2dipWithDefaultDensity(applicationContext, height.toFloat()) + ")")
+
+            }
+        }
+    }
+}
--- BaseDebug/src/main/java/com/bihe0832/android/base/debug/webview/DebugWebviewFragment.java
+++ BaseDebug/src/main/java/com/bihe0832/android/base/debug/webview/DebugWebviewFragment.java
@@ -5,18 +5,14 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsoluteLayout;
 import android.widget.TextView;
-
-import androidx.annotation.NonNull;
 
 import com.bihe0832.android.base.debug.R;
 import com.bihe0832.android.common.webview.CommonWebviewFragment;
+import com.bihe0832.android.common.webview.base.BaseWebviewFragment;
+import com.bihe0832.android.common.webview.log.MyBaseJsBridgeProxy;
 import com.bihe0832.android.lib.thread.ThreadManager;
-import com.bihe0832.android.lib.utils.ConvertUtils;
 import com.bihe0832.android.lib.utils.os.DisplayUtil;
-import com.tencent.smtt.sdk.ValueCallback;
-import com.tencent.smtt.sdk.WebChromeClient;
 import com.tencent.smtt.sdk.WebView;
 import com.tencent.smtt.sdk.WebViewClient;
 
@@ -28,6 +24,7 @@ import com.tencent.smtt.sdk.WebViewClient;
 public class DebugWebviewFragment extends CommonWebviewFragment {
     public static int HEIGHT = 50;
     private TextView textView = null;
+    private DebugJsBridgeProxy mBaseJsBridgeProxy = null;
 
     @Override
     protected void parseBundle(Bundle bundle, boolean isOnCreate) {
@@ -35,43 +32,45 @@ public class DebugWebviewFragment extends CommonWebviewFragment {
         mIntentUrl = "file:///android_asset/webview_test.html";
     }
 
-    @Override
-    protected void actionOnPageFinished(WebView view, String url) {
-        super.actionOnPageFinished(view, url);
-        mWebView.evaluateJavascript("javaScript:getNativeViewPosition()", new ValueCallback<String>() {
-            @Override
-            public void onReceiveValue(String value) {
-                ThreadManager.getInstance().runOnUIThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        AbsoluteLayout.LayoutParams params = (AbsoluteLayout.LayoutParams) textView.getLayoutParams();
-                        params.y = DisplayUtil.dip2pxWithDefaultDensity(getContext(), ConvertUtils.parseFloat(value, -1));
-                        textView.setLayoutParams(params);
-                        mWebView.loadUrl("javaScript:setNativeViewHeight(" + HEIGHT + ")");
-                    }
-                });
-            }
-        });
+    protected DebugJsBridgeProxy getJsBridgeProxy() {
+        if (mBaseJsBridgeProxy == null) {
+            mBaseJsBridgeProxy = new DebugJsBridgeProxy(mWebView, getActivity());
+        }
+        return mBaseJsBridgeProxy;
     }
 
     @Override
-    protected void initView(@NonNull View view) {
-        super.initView(view);
-        this.mRefreshable = false;
+    protected WebViewClient getWebViewClient() {
+        return new DebugWebViewClient(getJsBridgeProxy());
     }
 
-    @Override
-    protected void initWebview(View view, WebViewClient webViewClient, WebChromeClient webChromeClient) {
-        super.initWebview(view, webViewClient, webChromeClient);
-        textView = getTextView(getContext());
-        mWebView.addView(textView);
+    private class DebugWebViewClient extends BaseWebviewFragment.MyWebViewClient {
+
+        public DebugWebViewClient(MyBaseJsBridgeProxy jsBridge) {
+            super(jsBridge);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            ThreadManager.getInstance().runOnUIThread(new Runnable() {
+                @Override
+                public void run() {
+                    textView = getTextView(getContext());
+                    mWebView.addView(textView);
+                    mBaseJsBridgeProxy.setNativeView(textView);
+                }
+            });
+        }
     }
+
 
     public static final TextView getTextView(Context context) {
         TextView textView = new TextView(context);
         textView.setBackgroundColor(context.getResources().getColor(R.color.bihe0832_common_toast_background_color));
         textView.setText((CharSequence) "Zixie AAF Webview Debug TextView2 ");
         textView.setGravity(Gravity.CENTER);
+        textView.setVisibility(View.GONE);
         textView.setLayoutParams(new ViewGroup.MarginLayoutParams(-1, DisplayUtil.dip2px(context, (float) HEIGHT)));
         return textView;
     }
