4c13f15c21e9289fd55b2b60284d544a323285c3
子勰 <code@bihe0832.com>
2023-06-27 14:35:59 +0800

    icon 资源统一

--- Application/src/main/java/com/bihe0832/android/app/message/AAFMessageActivity.java
+++ Application/src/main/java/com/bihe0832/android/app/message/AAFMessageActivity.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
+import com.bihe0832.android.app.R;
 import com.bihe0832.android.app.router.RouterConstants;
 import com.bihe0832.android.framework.ui.main.CommonActivity;
 import com.bihe0832.android.lib.router.annotation.Module;
+import com.bihe0832.android.lib.theme.ThemeResourcesManager;
 
 
 @Module(RouterConstants.MODULE_NAME_MESSAGE)
@@ -14,7 +16,7 @@ public final class AAFMessageActivity extends CommonActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initToolbar("消息中心", true);
+        initToolbar(ThemeResourcesManager.INSTANCE.getString(R.string.settings_message_title), true);
         if (findFragment(AAFMessageListFragment.class) == null) {
             loadRootFragment(new AAFMessageListFragment());
         }
