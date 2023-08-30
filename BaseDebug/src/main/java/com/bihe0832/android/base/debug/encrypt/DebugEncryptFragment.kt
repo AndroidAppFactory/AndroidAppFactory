3c4be337a3c482084e55a2880206e1f088a814b7
子勰 <code@bihe0832.com>
2023-08-30 10:22:10 +0800

    精简调试层级

--- BaseDebug/src/main/java/com/bihe0832/android/base/debug/encrypt/DebugEncryptFragment.kt
+++ BaseDebug/src/main/java/com/bihe0832/android/base/debug/encrypt/DebugEncryptFragment.kt
@@ -1,22 +1,41 @@
 package com.bihe0832.android.base.debug.encrypt
 
+import android.view.View
+import com.bihe0832.android.common.debug.item.DebugItemData
 import com.bihe0832.android.common.debug.item.DebugTipsData
 import com.bihe0832.android.common.debug.module.DebugCommonFragment
 import com.bihe0832.android.lib.adapter.CardBaseModule
+import com.bihe0832.android.lib.utils.encrypt.RSAUtils
 
 open class DebugEncryptFragment : DebugCommonFragment() {
 
     override fun getDataList(): ArrayList<CardBaseModule> {
         return ArrayList<CardBaseModule>().apply {
-            add(DebugTipsData("各种加解密、数据完整性验证等"))
-            add(getDebugFragmentItemData("通用AES、RSA、MD5、SHA256验证等", DebugEncryptCommonFragment::class.java))
+            add(DebugTipsData("通用AES、RSA、MD5、SHA256验证等"))
+            add(DebugItemData("通用完整性校验（MD5、SHA256）", View.OnClickListener { testMessageDigest() }))
+            add(DebugItemData("RSA加解密调试", View.OnClickListener { rsaEncryptDebug() }))
             add(
-                getDebugFragmentItemData(
-                    "基于Android keystore 的AES、RSA",
-                    DebugEncryptWithKeystoreFragment::class.java,
+                DebugItemData(
+                    "使用 RSA ${RSAUtils.MOD_OAEP} 加密解密",
+                    View.OnClickListener { rsaEncrypt(RSAUtils.MOD_OAEP) },
                 ),
             )
-            add(getDebugFragmentItemData("基于具体应用场景的加解密方案", DebugEncryptSceneFragment::class.java))
+            add(
+                DebugItemData(
+                    "使用 RSA ${RSAUtils.MOD_PKCS_1} 加密解密",
+                    View.OnClickListener { rsaEncrypt(RSAUtils.MOD_PKCS_1) },
+                ),
+            )
+
+            add(DebugItemData("AES加解密调试", View.OnClickListener { aesEncryptDebug() }))
+            add(DebugItemData("使用 AES 加密解密", View.OnClickListener { aesEncrypt() }))
+            add(DebugTipsData("基于 Keystore 的加解密方案"))
+            add(DebugItemData("使用系统生成秘钥RSA加密解密", View.OnClickListener { androidRSAEncrypt() }))
+            add(DebugItemData("使用系统生成秘钥系统AES加密解密", View.OnClickListener { androidAESEncrypt() }))
+            add(DebugTipsData("具体应用场景加解密方案"))
+            add(DebugItemData("使用系统生成秘钥AES加密解密", View.OnClickListener { saveAES() }))
+            add(DebugItemData("使用本地公钥RSA加解密秘钥后AES加密解密", View.OnClickListener { saveData() }))
+            add(DebugItemData("数据公私钥签名验证", View.OnClickListener { checkSig() }))
         }
     }
 }
