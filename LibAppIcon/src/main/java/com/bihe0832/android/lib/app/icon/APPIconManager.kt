bddca5222ba05175b5f606c9991785e1200692f5
子勰 <code@bihe0832.com>
2025-08-29 09:49:05 +0800

    应用icon 切换优化

--- LibAppIcon/src/main/java/com/bihe0832/android/lib/app/icon/APPIconManager.kt
+++ LibAppIcon/src/main/java/com/bihe0832/android/lib/app/icon/APPIconManager.kt
@@ -2,12 +2,14 @@ package com.bihe0832.android.lib.app.icon
 
 import android.content.ComponentName
 import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_DEFAULT
 import android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_DISABLED
 import android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+import android.text.TextUtils
 import com.bihe0832.android.lib.lifecycle.ApplicationObserver
 import com.bihe0832.android.lib.log.ZLog
+import com.bihe0832.android.lib.thread.ThreadManager
 import java.util.concurrent.ConcurrentHashMap
 
 
@@ -21,99 +23,147 @@ import java.util.concurrent.ConcurrentHashMap
 object APPIconManager {
 
     private const val TAG = "APPIconManager"
-    private var mAPPStatusChangeListener: ApplicationObserver.APPStatusChangeListener? = null
 
-    init {
-        ApplicationObserver.addDestoryListener(object : ApplicationObserver.APPDestroyListener {
-            override fun onAllActivityDestroyed() {
-                mAPPStatusChangeListener?.let {
-                    ApplicationObserver.removeStatusChangeListener(it)
-                }
-                ApplicationObserver.removeDestoryListener(this)
-            }
-
-        })
-    }
-
-    fun changeAppIcon(context: Context, enableAlias: String, allAlias: List<String>) {
-        changeAppIcon(context, listOf(enableAlias), allAlias)
-    }
-
-    fun changeAppIcon(context: Context, enableAlias: List<String>, allAlias: List<String>) {
-        ZLog.d(TAG, "enableAlias:" + enableAlias.joinToString(","))
-        ZLog.d(TAG, "allAlias:" + allAlias.joinToString(","))
-        var needChange = false
-        val data = getAliasState(context.applicationContext, allAlias)
-        enableAlias.forEach {
-            if (data.containsKey(it) && data[it] == COMPONENT_ENABLED_STATE_ENABLED) {
-                ZLog.d(TAG, "enableAlias: $it has Enabled")
-            } else {
-                needChange = true
-            }
+    fun changeAppIcon(context: Context, enableAlias: String, allAlias: List<String>, delay: Long) {
+        if (TextUtils.isEmpty(enableAlias)) {
+            ZLog.d(TAG, "changeAppIcon: skip, enableAlias is empty")
+        } else {
+            changeAppIconState(
+                context,
+                listOf(enableAlias),
+                allAlias.filter { !enableAlias.contains(it) },
+                delay
+            )
         }
+    }
+
+    fun changeAppIconState(
+        context: Context,
+        enableAlias: List<String>,
+        disableAlias: List<String>,
+        times: Long
+    ) {
+
+        ZLog.d(TAG, "enableAlias:" + enableAlias.joinToString(","))
+        ZLog.d(TAG, "disableAlias:" + disableAlias.joinToString(","))
+        val needChange = needChange(context, enableAlias, disableAlias)
+        ZLog.d(TAG, "needChange:$needChange")
         if (needChange) {
             ZLog.d(TAG, "enableAlias add Listener")
-            mAPPStatusChangeListener = object : ApplicationObserver.APPStatusChangeListener {
+            ApplicationObserver.addStatusChangeListener(object :
+                ApplicationObserver.APPStatusChangeListener {
                 override fun onBackground() {
                     ZLog.d(TAG, "onBackground")
-                    ZLog.d(TAG, "enableAlias:" + enableAlias.joinToString(","))
-                    ZLog.d(TAG, "allAlias:" + allAlias.joinToString(","))
-                    if (enableAlias.isNotEmpty()) {
-                        changeAppIconAction(context.applicationContext, enableAlias, allAlias)
-                        getAliasState(context.applicationContext, allAlias).forEach {
-                            ZLog.d(TAG, it.key + " state:" + it.value)
+                    ThreadManager.getInstance().start({
+                        ZLog.d(TAG, "enableAlias:" + enableAlias.joinToString(","))
+                        ZLog.d(TAG, "disableAlias:" + disableAlias.joinToString(","))
+                        val needChangeWhenBack = needChange(context, enableAlias, disableAlias)
+                        ZLog.d(TAG, "needChangeWhenBack:$needChangeWhenBack")
+                        if (needChangeWhenBack) {
+                            changeAppIconAction(
+                                context.applicationContext,
+                                enableAlias,
+                                disableAlias
+                            )
+                            getAliasState(context.applicationContext, enableAlias).forEach {
+                                ZLog.d(TAG, "enableAlias:" + it.key + " state:" + it.value)
+                            }
+                            getAliasState(context.applicationContext, disableAlias).forEach {
+                                ZLog.d(TAG, "disableAlias:" + it.key + " state:" + it.value)
+                            }
+                            ZLog.d(TAG, "remove Listener after change")
+                            ApplicationObserver.removeStatusChangeListener(this)
+                        } else {
+                            ZLog.d(TAG, "remove Listener no change")
+                            ApplicationObserver.removeStatusChangeListener(this)
                         }
-                    }
+                    }, times)
+
                 }
 
                 override fun onForeground() {
                     ZLog.d(TAG, "onForeground")
                 }
-
-            }
-            mAPPStatusChangeListener?.let {
-                ApplicationObserver.addStatusChangeListener(it)
-            }
-        }else{
+            })
+        } else {
             ZLog.d(TAG, "enableAlias skip")
         }
+    }
 
+    private fun needChange(
+        context: Context,
+        enableAlias: List<String>,
+        disableAlias: List<String>
+    ): Boolean {
+        val enableData = getAliasState(context.applicationContext, enableAlias)
+        enableData.forEach {
+            when (it.value) {
+                COMPONENT_ENABLED_STATE_ENABLED -> {
+                    ZLog.d(TAG, "enableAlias needChange: $it has Enabled")
+                }
 
+                else -> {
+                    return true
+                }
+            }
+        }
+        val disableData = getAliasState(context.applicationContext, disableAlias)
+        disableData.forEach {
+            when (it.value) {
+                COMPONENT_ENABLED_STATE_ENABLED, COMPONENT_ENABLED_STATE_DEFAULT -> {
+                    ZLog.d(TAG, "disableAlias needChange: $it has Enabled")
+                    return true
+                }
+
+                else -> {
+                    ZLog.d(TAG, "disableAlias needChange: $it is " + it.value)
+                }
+            }
+
+        }
+        return false
     }
 
     private fun changeAppIconAction(
         context: Context,
         enableAlias: List<String>,
-        allAlias: List<String>
+        disableAlias: List<String>
     ) {
         val pm = context.packageManager
         val packageName = context.packageName
 
-        enableAlias.forEach { alias ->
-            pm.setComponentEnabledSetting(
-                ComponentName(packageName, alias),
-                COMPONENT_ENABLED_STATE_ENABLED,
-                PackageManager.DONT_KILL_APP
-            )
-        }
+        val enableData = getAliasState(context.applicationContext, enableAlias)
+        enableData.forEach {
+            when (it.value) {
+                COMPONENT_ENABLED_STATE_ENABLED -> {
+                    ZLog.d(TAG, "enableAlias: $it has Enabled")
+                }
 
-        allAlias.filter { !enableAlias.contains(it) }.forEach { alias ->
-            pm.setComponentEnabledSetting(
-                ComponentName(packageName, alias),
-                COMPONENT_ENABLED_STATE_DISABLED,
-                PackageManager.DONT_KILL_APP
-            )
+                else -> {
+                    pm.setComponentEnabledSetting(
+                        ComponentName(packageName, it.key),
+                        COMPONENT_ENABLED_STATE_ENABLED,
+                        PackageManager.DONT_KILL_APP
+                    )
+                }
+            }
         }
-        refreshLauncher(context)
-    }
+        val disableData = getAliasState(context.applicationContext, disableAlias)
+        disableData.forEach {
+            when (it.value) {
+                COMPONENT_ENABLED_STATE_ENABLED, COMPONENT_ENABLED_STATE_DEFAULT -> {
+                    pm.setComponentEnabledSetting(
+                        ComponentName(packageName, it.key),
+                        COMPONENT_ENABLED_STATE_DISABLED,
+                        PackageManager.DONT_KILL_APP
+                    )
+                }
 
-    private fun refreshLauncher(context: Context) {
-        val intent = Intent(Intent.ACTION_MAIN).apply {
-            addCategory(Intent.CATEGORY_HOME)
-            addCategory(Intent.CATEGORY_DEFAULT)
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                else -> {
+                    ZLog.d(TAG, "disableAlias: $it is " + it.value)
+                }
+            }
         }
-        context.startActivity(intent)
     }
 
     fun getAliasState(context: Context, aliases: List<String>): ConcurrentHashMap<String, Int> {
