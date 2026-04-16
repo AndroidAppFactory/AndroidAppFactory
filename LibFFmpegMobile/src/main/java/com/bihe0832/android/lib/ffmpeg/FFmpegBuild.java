fe6760bf6212e903f8b0b57c0865aafdfc730d01
子勰 <code@bihe0832.com>
2026-04-16 10:07:03 +0800

    style(lint): 修复代码规范问题

--- LibFFmpegMobile/src/main/java/com/bihe0832/android/lib/ffmpeg/FFmpegBuild.java
+++ LibFFmpegMobile/src/main/java/com/bihe0832/android/lib/ffmpeg/FFmpegBuild.java
@@ -3,8 +3,7 @@ package com.bihe0832.android.lib.ffmpeg;
 /**
  * 总结怎么通过 [mobile-ffmpeg](https://github.com/tanersener/mobile-ffmpeg) 构建自定义扩展的 ffmpeg
  *
- * 官方构建脚本：https://github.com/tanersener/mobile-ffmpeg/wiki/android.sh
+ * <p>官方构建脚本：https://github.com/tanersener/mobile-ffmpeg/wiki/android.sh
  *
- * AAF 构建仓库：https://github.com/zixieTools/mobile-ffmpeg
- *
+ * <p>AAF 构建仓库：https://github.com/zixieTools/mobile-ffmpeg
  */
